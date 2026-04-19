d055f24fa05425cecb597b8a195ddb6e66042f55
582388360 <13671295@qq.com>
2026-04-19 17:24:08 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,26 +1,19 @@
-window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});// very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});// Pake 新窗口兼容修复 - 稳定无bug版
 const hookClick = (e) => {
-    const origin = e.target.closest('a')
-    const isBaseTargetBlank = document.querySelector(
-        'head base[target="_blank"]'
-    )
-    console.log('origin', origin, isBaseTargetBlank)
-    if (
-        (origin && origin.href && origin.target === '_blank') ||
-        (origin && origin.href && isBaseTargetBlank)
-    ) {
-        e.preventDefault()
-        console.log('handle origin', origin)
-        location.href = origin.href
-    } else {
-        console.log('not handle origin', origin)
-    }
-}
+  const link = e.target.closest('a');
+  if (!link || !link.href) return;
 
-window.open = function (url, target, features) {
-    console.log('open', url, target, features)
-    location.href = url
-}
+  const isBlank = link.target === '_blank';
+  const baseBlank = document.querySelector('head base[target="_blank"]');
 
-document.addEventListener('click', hookClick, { capture: true })
+  if (isBlank || baseBlank) {
+    e.preventDefault();
+    window.location.href = link.href;
+  }
+};
+
+window.open = (url) => {
+  window.location.href = url;
+};
+
+document.addEventListener('click', hookClick, { capture: true });
